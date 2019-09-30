896fcac229068034aa1f1a767a8bb129f947433c
tmwinjil <takundalm@outlook.com>
2019-09-29 18:25:53 -0700

    Completed assignment1 to specification

--- assign1/assign1_part2/shell.c
+++ assign1/assign1_part2/shell.c
@@ -14,10 +14,12 @@
 /* DEFINE SECTION */
 #define SHELL_BUFFER_SIZE 256   /* Size of the Shell input buffer        */
 #define SHELL_MAX_ARGS 8        /* Maximum number of arguments parsed    */
+#define HISTORY_SIZE 9			/* Maximum size of command history		 */
+#define MAX_SHELL_LEVEL 3		/* Maximum level of subshell			 */
 
 /* VARIABLE SECTION */
 enum { STATE_SPACE, STATE_NON_SPACE };	/* Parser states */
-
+int shell_level; /*Level of shell starting at 1*/
 
 int imthechild(const char *path_to_exec, char *const args[])
 {
@@ -37,7 +39,7 @@ void imtheparent(pid_t child_pid, int run_in_background)
 		        "  Parent says 'run_in_background=1 ... so we're not waiting for the child'\n");
 		return;
 	}
-	waitpid(child_pid, &child_return_val);
+	waitpid(child_pid,&child_return_val,WUNTRACED);
 	/* Use the WEXITSTATUS to extract the status code from the return value */
 	child_error_code = WEXITSTATUS(child_return_val);
 	fprintf(stderr,
@@ -62,18 +64,19 @@ int main(int argc, char **argv)
 {
 	pid_t shell_pid, pid_from_fork;
 	int n_read, i, exec_argc, parser_state, run_in_background;
-	int command_count = 1;
-	int history_size = SHELL_MAX_ARGS;
+	shell_level = 1;
 	/* buffer: The Shell's input buffer. */
 	char buffer[SHELL_BUFFER_SIZE];
 	/* exec_argv: Arguments passed to exec call including NULL terminator. */
 	char *exec_argv[SHELL_MAX_ARGS + 1];
-	/*Path: Appended to the front of commands with no path declared*/
+	/*command_count: Counts number of commands executed(whether valid or not)*/
+	int command_count = 1;
+	/*path: Appended to the front of commands with no path declared*/
 	const char *path = "/bin/";
 	/*command: Result of appending path should it be necessary*/
 	char command[15];
 	/*history: Dynamically allocated history of commands(max 9 commands)*/
-	char **history = malloc((SHELL_MAX_ARGS + 1) * sizeof(char*));
+	char **history = malloc(HISTORY_SIZE * sizeof(char*));
 	
 	/* Entrypoint for the testrunner program */
 	if (argc > 1 && !strcmp(argv[1], "-test")) {
@@ -97,10 +100,10 @@ int main(int argc, char **argv)
 		buffer[n_read - run_in_background - 1] = '\n';//TK:Places end line character after command or as replacement for '&'
 
 		//Check to see if the buffer holds a recall instruction
-		if(*buffer == '!') {
+		if(*buffer == '!' && command_count <= HISTORY_SIZE) {
 			int index = atoi(buffer + 1);
 			if (index >= command_count || index <=0) {
-						fprintf(stderr,"Not valid");
+						fprintf(stderr,"Not valid\n");
 						continue;
 					}else{
 						strcpy(buffer, history[index - 1]);
@@ -156,7 +159,29 @@ int main(int argc, char **argv)
 				fprintf(stderr, "cd: failed to chdir %s\n", exec_argv[1]);	
 			/* End alternative: exit(EXIT_SUCCESS);}*/ 
 
-		} else {
+		} else if(!strcmp(exec_argv[0],"sub")) { //"sub" command
+			if(shell_level == MAX_SHELL_LEVEL) { //Cannot invoke a subshell from level 3
+				fprintf(stderr, "Too deep!\n");
+				continue;
+			}
+			pid_from_fork = fork();
+			if (pid_from_fork < 0) {
+				/* Error: fork() failed.  Unlikely, but possible (e.g. OS *
+				 * kernel runs out of memory or process descriptors).     */
+				fprintf(stderr, "fork failed\n");
+				continue;
+			}
+			if (pid_from_fork == 0) {
+				shell_level++; //increment the shell level
+				shell_pid++;  //Increment the forked shell's pid as it would normally be shown for the parent
+				command_count--; //maintain command count when forking to subshell
+				free(history[command_count - 1]);
+			}else{
+				imtheparent(pid_from_fork, run_in_background);
+				continue;
+				/* Parent will continue around the loop. */
+			}
+		}else {
 		/* Execute Commands */
 			/* Try replacing 'fork()' with '0'.  What happens? */
 			pid_from_fork = fork();
