4bb4c8cd1f03a702e96d135d777481034bd3e17b
tmwinjil <takundalm@outlook.com>
2019-11-16 13:14:54 -0800

    Answered most questions to best of ability. Work still needs to be done on question 1 and 10

--- assign3/assign3_part1/sched_impl.h
+++ assign3/assign3_part1/sched_impl.h
@@ -7,14 +7,12 @@ struct thread_info {
 	list_t* queue;
 	list_elem_t* queueData;
 	sem_t runWorker;//Semaphore to activate or deactivate worker thread.
-	/*...Fill this in...*/
 };
 
 struct sched_queue {
 	list_elem_t* currentWorker;
 	//Insert all semaphores for queue here
 	list_t* list;
-	/*...Fill this in...*/
 };
 
 #endif /* __SCHED_IMPL__H__ */
