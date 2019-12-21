2dc50fc2476f4d92234a8a494eea7d96b70b0234
tmwinjil <takundalm@outlook.com>
2019-12-20 19:04:23 -0800

    Started modifications to part 1 (for fun)

--- assign4/assign4_part1/fileio.c
+++ assign4/assign4_part1/fileio.c
@@ -16,48 +16,64 @@
 
 int file_read(char *path, int offset, void *buffer, size_t bufbytes)
 {
+    if (!path || !buffer || bufbytes < 1 || offset < 0)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int file_info(char *path, void *buffer, size_t bufbytes)
 {
     if (!path || !buffer || bufbytes < 1)
-	return IOERR_INVALID_ARGS;
+	    return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int file_write(char *path, int offset, void *buffer, size_t bufbytes)
 {
+    if (!path || !buffer || bufbytes < 1 || offset < 0)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int file_create(char *path, char *pattern, int repeatcount)
 {
+    if (!path || !pattern || repeatcount < 1)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int file_remove(char *path)
 {
+    if (!path)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int dir_create(char *path)
 {
+    if (!path)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int dir_list(char *path, void *buffer, size_t bufbytes)
 {
+    if (!path || !buffer || bufbytes < 1)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 
 int file_checksum(char *path)
 {
+    if (!path)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
 
 int dir_checksum(char *path)
 {
+    if (!path)
+        return IOERR_INVALID_ARGS;
     return IOERR_NOT_YET_IMPLEMENTED;
 }
