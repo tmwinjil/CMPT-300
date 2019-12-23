9018752f0ba7c415ed4839c072996a5c98d77fb7
tmwinjil <takundalm@outlook.com>
2019-12-22 21:13:32 -0800

    Completed first step for read, write and info

--- assign4/assign4_part1/fileio.c
+++ assign4/assign4_part1/fileio.c
@@ -18,31 +18,52 @@ int file_read(char *path, int offset, void *buffer, size_t bufbytes)
 {
     if (!path || !buffer || bufbytes < 1 || offset < 0)
         return IOERR_INVALID_ARGS; 
-    int fd, bytesRd;
-    fd = r_open2(path, O_RDONLY);
+
+    int fd = r_open2(path, O_RDONLY);
     if (fd == -1)//file failed to open
         return IOERR_INVALID_PATH;
     if(lseek(fd, (off_t) offset, SEEK_SET) == (off_t) -1)
         return IOERR_POSIX;
-    bytesRd = r_read(fd, buffer, bufbytes);
+    size_t bytesRd = r_read(fd, buffer, bufbytes);
     if (bytesRd == -1)
        return IOERR_POSIX;
     else
-        return bytesRd;
+        return (int) bytesRd;
 }
 
 int file_info(char *path, void *buffer, size_t bufbytes)
 {
     if (!path || !buffer || bufbytes < 1)
 	    return IOERR_INVALID_ARGS;
-    return IOERR_NOT_YET_IMPLEMENTED;
+    struct stat info;
+    char type = 'X';
+    if(stat(path, &info) == -1)
+        return IOERR_POSIX;
+
+    if (S_ISREG(info.st_mode))
+        type = 'f';
+    else if(S_ISDIR(info.st_mode))
+        type = 'd';
+        
+    sprintf((char*)buffer,"Size:%ld Accessed:%ld Modified:%ld Type %c",info.st_size, info.st_atime, info.st_mtime, type);
+    return 0;
 }
 
 int file_write(char *path, int offset, void *buffer, size_t bufbytes)
 {
     if (!path || !buffer || bufbytes < 1 || offset < 0)
-        return IOERR_INVALID_ARGS;
-    return IOERR_NOT_YET_IMPLEMENTED;
+        return IOERR_INVALID_ARGS; 
+
+    int fd = r_open2(path, O_WRONLY);
+    if (fd == -1)//file failed to open
+        return IOERR_INVALID_PATH;
+    if(lseek(fd, (off_t) offset, SEEK_SET) == (off_t) -1)
+        return IOERR_POSIX;
+    size_t bytesRd = r_write(fd, buffer, bufbytes);
+    if (bytesRd == -1 || !r_close(fd))
+       return IOERR_POSIX;
+    else
+        return (int) bytesRd;
 }
 
 int file_create(char *path, char *pattern, int repeatcount)
